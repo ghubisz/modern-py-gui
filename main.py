89be62bfc4745fe8a4f2008305ea031f9e0f60c2
ghubisz <gab.op.pl@op.pl>
2023-12-29 12:29:43 +0100

    app initialized, downloaded button not working yet

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import tkinter
 import customtkinter
 from pytube import YouTube
 
+def startDownload():
+    try:
+        ytLink = link.get()
+        ytObject = YouTube(ytLink)
+        video = ytObject.streams.get_highest_resolution()
+        video.download()
+    except:
+        print("Youtube link is invalid")
+    finishLabel.configure(text="Downloaded!")
+
 #System Settings
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
@@ -10,6 +20,23 @@ customtkinter.set_default_color_theme("blue")
 app = customtkinter.CTk()
 app.geometry("720x480")
 app.title("Video Downloader")
+
+#Adding UI Elements
+title = customtkinter.CTkLabel(app, text="Insert a youtube link")
+title.pack(padx=10, pady=10)
+
+#Link input
+url_var = tkinter.StringVar()
+link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
+link.pack()
+
+#Finished Downloading
+finishLabel = customtkinter.CTkLabel(app, text="")
+finishLabel.pack()
+
+#Download Button
+download = customtkinter.CTkButton(app, text="Download", command=startDownload)
+download.pack(padx=10, pady=10)
 
 #Run app
 app.mainloop()
